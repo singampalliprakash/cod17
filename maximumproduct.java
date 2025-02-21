efa312bf504003d37746c9403b8b29df74a0e647
Jnana prakash Singampalli <jnanaprakash2002@gmail.com>
2025-02-22 00:16:40 +0530

    maximumproduct.java

--- maximumproduct.java
+++ maximumproduct.java
@@ -1,20 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         int[] nums = {2, 3, -2, 4};
-        
-        int maxProd = nums[0], minProd = nums[0], result = nums[0];
-        
+        int maxProd = nums[0];  
+        int result = maxProd;
+
         for (int i = 1; i < nums.length; i++) {
-            if (nums[i] < 0) {
-                int temp = maxProd;
-                maxProd = minProd;
-                minProd = temp;
+            if (nums[i] > 0) {
+                maxProd *= nums[i]; 
+            } else {
+                maxProd = nums[i];
             }
-            maxProd = Math.max(nums[i], maxProd * nums[i]);
-            minProd = Math.min(nums[i], minProd * nums[i]);
-            result = Math.max(result, maxProd);
+            result = Math.max(result, maxProd);  
         }
-        
-        System.out.println(result);
+
+        System.out.println(result);  
     }
 }
